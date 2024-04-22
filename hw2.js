65ecd184329c3868c6ea5e906e0a2906f417573d
xeniayefimova <162689466+xeniayefimova@users.noreply.github.com>
2024-04-22 21:45:35 +0200

    Add files via upload

---
+++ hw2.js
@@ -0,0 +1,28 @@
+var batterConsistency
+batterConsistency = "сметана" //string
+
+var patelniaTemperature
+patelniaTemperature = "141" //number
+
+const cookingTime = "12 годин" //string
+
+let flipCount
+flipCount = "12" //number
+
+let ingredientList
+ingredientList = "eggs, milk, water, suger" //string
+
+const batterVolume = "750" //number
+
+let servingSize
+servingSize = '350' //number
+
+let toppingChoice
+toppingChoice = 'strawberry' //string
+
+let remainingBatter
+remainingBatter = true //boolean
+
+var patelniaOilLevel //undefined
+
+
